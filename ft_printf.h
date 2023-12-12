333785a280197143ae209fff8d78c08cfbb85a21
Nour Eddine Ait Bouguarri <nait-bou@e1r11p1.1337.ma>
2023-12-12 12:44:15 +0100

    k

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nait-bou <nait-bou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 19:00:57 by nait-bou          #+#    #+#             */
-/*   Updated: 2023/12/12 11:39:59 by nait-bou         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:42:16 by nait-bou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 # include <unistd.h>
 # include <stddef.h>
 
-int	    ft_printf(const char *format, ...);
-int	    ft_print_char(char c);
-int	    ft_print_str(char *str);
-int	    ft_print_nbr(int nbr);
-int	    ft_print_pointer(unsigned long value);
-int	    ft_print_hex(unsigned int n, int d);
-int	    ft_print_unsigned(unsigned int nbr);
+int		ft_printf(const char *format, ...);
+int		ft_print_char(char c);
+int		ft_print_str(char *str);
+int		ft_print_nbr(int nbr);
+int		ft_print_pointer(unsigned long value);
+int		ft_print_hex(unsigned int n, int d);
+int		ft_print_unsigned(unsigned int nbr);
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 void	ft_putnbr(int n);
-int     ft_strlen(const char *s);
+int		ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 
 #endif
